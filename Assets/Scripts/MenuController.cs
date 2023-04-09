b3e0e2d5e3f8acceb0b12f6cae8c8ea5287844c1
Shane <st20189889@outlook.cardiffmet.ac.uk>
2023-04-09 22:45:55 +0100

    CatLamp Port
    bye bye bitbucket

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] private GameObject menu;
+    //[SerializeField] private GameObject ccMenu;
+    //[SerializeField] private GameObject particleMenu;
+    //[SerializeField] private GameObject objectMenu;
+
+    private bool toggle = false;
+
+    void hidePanel()
+    {
+        menu.SetActive(false);
+    }
+
+    void unhidePanel()
+    {
+        menu.SetActive(true);
+    }
+
+    public void Toggle()
+    {
+        if (!toggle)
+        {
+            toggle = true;
+            unhidePanel();
+        }
+        else if (toggle)
+        {
+            toggle = false;
+            hidePanel();
+        }
+    }
+}
